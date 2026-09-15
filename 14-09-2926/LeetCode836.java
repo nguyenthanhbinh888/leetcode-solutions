4d7224a998501b34c4810d99436e7f05444c62ee
Nguyễn Thanh Bình <103369686+nguyenthanhbinh888@users.noreply.github.com>
2026-09-15 08:03:34 +0700

    LeetCode 836 – Rectangle Overlap

---
+++ 14-09-2926/LeetCode836.java
@@ -0,0 +1,20 @@
+public class LeetCode836 {
+
+    public static boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+
+        return rec1[0] < rec2[2]
+            && rec1[2] > rec2[0]
+            && rec1[1] < rec2[3]
+            && rec1[3] > rec2[1];
+    }
+
+    public static void main(String[] args) {
+
+        int[] rec1 = {0, 0, 2, 2};
+        int[] rec2 = {1, 1, 3, 3};
+
+        boolean result = isRectangleOverlap(rec1, rec2);
+
+        System.out.println(result);
+    }
+}
